267a27cc36060679bb7ac43a32e8b963bc5c7137
OwenIsFried <stormfry113@gmail.com>
2026-05-05 11:53:13 -0400

    Update weather.py
    Added vulnerability comments

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests
 import sqlite3
 
+# vulnerability 2 - hardcoded/public api key
 API_KEY = "VEhpcyBpcyBhIGZha2Uga2V5LCBidXQgbWF5YmUgbG9va3MgbGlrZSBvbmU="
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 DB = None
@@ -14,13 +15,16 @@ def get_weather(city):
     response = requests.get(BASE_URL, params=params)
 
     #collect from 3rd party API
+    # vulnerability 3 - not validating api info (api could have been compromised.)
     data = response.json()
     
     city_name = data["name"]
     temp = data["main"]["temp"]
 
     #insert into DB
+    # vulnerability 4 - not utilizing a prepare statement (possible sqli)
     c = DB.cursor()
+    # vulnerability 5 (depends on the situation) - not hashing values inside of the database
     c.execute("INSERT INTO history (city, temp) VALUES (" + city_name +", " + temp + ")")
     DB.commit()
 
@@ -33,3 +37,5 @@ if __name__ == "__main__":
     #collect from user
     city = input("Enter city name: ")
     get_weather(city)
+
+    # vulnerability 1 - not validating user input
